0daf67fe8dd5890994c2f5f6c0a78af6778e1450
Josecastro-22 <73858107+Josecastro-22@users.noreply.github.com>
2023-11-26 20:34:41 -0500

    Your descriptive commit message here

---
+++ tracing.js
@@ -0,0 +1,40 @@
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { trace } = require("@opentelemetry/api");
+// Instrumentations
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+// Exporter
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+
+module.exports = (serviceName) => {
+  const jaegerExporter = new JaegerExporter({
+    serviceName: serviceName,
+    endpoint: 'http://localhost:14268/api/traces',
+  });
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  provider.addSpanProcessor(new BatchSpanProcessor(jaegerExporter));
+  provider.register();
+
+  registerInstrumentations({
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+      new MongoDBInstrumentation(),
+    ],
+    tracerProvider: provider,
+  });
+
+  return trace.getTracer(serviceName);
+};
+
